6dd83250b44ca8178c04f343c3c833ae2bf607d8
Juan Morales <juanmr2311@gmail.com>
2022-02-18 18:40:07 -0500

    Implementación del micro servicio User

---
+++ service-users/src/main/java/co/com/poli/users/controller/UserController.java
@@ -0,0 +1,53 @@
+package co.com.poli.users.controller;
+
+import co.com.poli.users.entities.User;
+import co.com.poli.users.helpers.Response;
+import co.com.poli.users.helpers.ResponseBuilder;
+import co.com.poli.users.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final ResponseBuilder builder;
+
+    @PostMapping
+    public Response save(@RequestBody User user){
+        userService.save(user);
+        return builder.success(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Response delete(@PathVariable("id") Long id) {
+        User user = userService.findById(id);
+        if(user == null){
+            return builder.failed("Not Found");
+        }
+        userService.delete(user);
+        return builder.success(user);
+    }
+
+    @GetMapping
+    public Response findAll(){
+        List<User> users = userService.findAll();
+        if(users == null){
+            return builder.failed("Users is Empty");
+        }
+        return builder.success(users);
+    }
+
+    @GetMapping("/{id}}")
+    public Response findById(@PathVariable("id") Long id){
+        User user = userService.findById(id);
+        if(user == null){
+            return builder.failed("Not Found");
+        }
+        return builder.success(user);
+    }
+}
--- service-users/src/main/java/co/com/poli/users/entities/User.java
+++ service-users/src/main/java/co/com/poli/users/entities/User.java
@@ -1,14 +1,12 @@
 package co.com.poli.users.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Objects;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 @AllArgsConstructor
